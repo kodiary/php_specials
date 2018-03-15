611ff314e15e1c6c53329ea6fca4886102d8df64
Anwar Ali <anwar@Anwars-MacBook-Pro.local>
2018-03-15 19:30:02 +0545

    String Manipulation

---
+++ string.php
@@ -0,0 +1,56 @@
+<?php
+//$str = "This Is A Random STRing";
+/*$str = str_replace('This Is A Random STRing','This Is A Random String',$str);//Replacing a String in the parent String
+echo $str;*/
+
+/*
+$sub = substr($str,5,7); // Grab the substring from the parent string
+echo $sub;
+*/
+
+/*
+$str = "<b>My Name is <i>Anwar Ali</i></b>";
+//echo $str;
+$stripped = strip_tags($str,'<b>'); // Strip out all the HTML tags from the parent string except for the mentioned tags
+echo $stripped;
+*/
+
+/*
+$str = strtoupper($str); // Converts the entire letters of the string to uppercase
+echo $str;
+*/
+/*
+$str = strtolower($str); // Converts the entire letters of the string to lowercase
+echo $str;
+*/
+/*
+$str = "anwar";
+$str = ucfirst($str);// Converts the first letter of the string to uppercase
+echo $str;
+*/
+/*
+if(isset($_POST['description']))
+{
+	$desc = $_POST['description'];
+	echo nl2br($desc); // If you want to covert the new line to <br>
+}
+?>
+<form action="" method="post">
+	<textarea name="description"></textarea><br>
+	<input type="submit" value="Submit">
+</form>
+*/
+/*
+$str = "Hello, Hi, What's Up, How is it going?";
+$arr = explode(',',$str); // separate a string with reference to a character and place them in array
+$new_str = implode('_',$arr); // Joing the values of array to a string with reference to a character.
+echo $new_str;
+*/
+/*
+$str = '   A b c d    ';
+echo trim($str);// To remove the unnecessary white spaces
+*/
+
+
+
+
