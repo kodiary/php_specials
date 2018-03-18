f90e77b79bcd18aef1aedf4cd4e526266e2c4731
Anwar Ali <anwar@Anwars-MacBook-Pro.local>
2018-03-18 21:54:55 +0545

    commenting database

--- php_specials.sql
+++ php_specials.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 05, 2018 at 02:00 PM
+-- Generation Time: Mar 18, 2018 at 04:09 PM
 -- Server version: 5.6.37
 -- PHP Version: 7.1.8
 
@@ -19,6 +19,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `php_specials`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `comment` text NOT NULL,
+  `comment_date` datetime NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `name`, `email`, `comment`, `comment_date`) VALUES
+(1, 'Anwar Ali', 'info@kodiary.com', 'Test comment Goes here', '2018-03-18 14:03:02'),
+(2, 'John', 'john@kodiary.com', 'Another Comment Goes here', '2018-03-18 14:03:29'),
+(3, 'Andrew', 'andrew@kodiary.com', 'Andrew''s comment goes here', '2018-03-18 14:04:30'),
+(4, 'James', 'james@kodiary.com', 'James'' comment goes here', '2018-03-18 14:05:30'),
+(5, 'Jacob', 'jacob@kodiary.com', 'Jacob''s comment goes here', '2018-03-18 14:09:25');
 
 -- --------------------------------------------------------
 
@@ -61,6 +86,40 @@ INSERT INTO `news` (`id`, `title`, `description`, `author`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `products`
+--
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `price` float NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `revisions`
+--
+
+CREATE TABLE IF NOT EXISTS `revisions` (
+  `id` int(11) NOT NULL,
+  `f1` varchar(255) NOT NULL,
+  `f2` varchar(255) NOT NULL,
+  `f3` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `revisions`
+--
+
+INSERT INTO `revisions` (`id`, `f1`, `f2`, `f3`) VALUES
+(1, 'abc', 'cde', 'efg');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -69,7 +128,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `gender` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -89,11 +148,18 @@ INSERT INTO `users` (`id`, `name`, `email`, `gender`) VALUES
 (11, 'Dabria', 'dabria@gmail.com', 'female'),
 (12, 'David', 'david@gmail.com', 'male'),
 (13, 'Dallas', 'dallas@gmail.com', 'female'),
-(14, 'Dame', 'dame@gmail.com', 'male');
+(14, 'Dame', 'dame@gmail.com', 'male'),
+(15, 'test user', 'test@test.com', 'female');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `images`
@@ -108,6 +174,18 @@ ALTER TABLE `news`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `revisions`
+--
+ALTER TABLE `revisions`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -117,6 +195,11 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `images`
 --
@@ -128,10 +211,20 @@ ALTER TABLE `images`
 ALTER TABLE `news`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `revisions`
+--
+ALTER TABLE `revisions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
